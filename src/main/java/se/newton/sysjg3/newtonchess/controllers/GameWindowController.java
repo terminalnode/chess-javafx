f160325108a61bb1ddefd916c79d8562bb05e7a0
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-04-04 19:49:03 +0200

    add initialization of all the squares to gamewindowcontroller

--- src/main/java/se/newton/sysjg3/newtonchess/controllers/GameWindowController.java
+++ src/main/java/se/newton/sysjg3/newtonchess/controllers/GameWindowController.java
@@ -1,97 +1,56 @@
 package se.newton.sysjg3.newtonchess.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import se.newton.sysjg3.newtonchess.chesscomponents.Square;
+import se.newton.sysjg3.newtonchess.chesscomponents.pieces.Piece;
 
+/**
+ * The class that holds the board and lets you play the game.
+ * @author Alexander Rundberg
+ */
 public class GameWindowController extends GenericController {
-  // Row 1
-  @FXML VBox sq00;
-  @FXML VBox sq01;
-  @FXML VBox sq02;
-  @FXML VBox sq03;
-  @FXML VBox sq04;
-  @FXML VBox sq05;
-  @FXML VBox sq06;
-  @FXML VBox sq07;
-
-  // Row 2
-  @FXML VBox sq10;
-  @FXML VBox sq11;
-  @FXML VBox sq12;
-  @FXML VBox sq13;
-  @FXML VBox sq14;
-  @FXML VBox sq15;
-  @FXML VBox sq16;
-  @FXML VBox sq17;
-
-  // Row 3
-  @FXML VBox sq20;
-  @FXML VBox sq21;
-  @FXML VBox sq22;
-  @FXML VBox sq23;
-  @FXML VBox sq24;
-  @FXML VBox sq25;
-  @FXML VBox sq26;
-  @FXML VBox sq27;
-
-  // Row 4
-  @FXML VBox sq30;
-  @FXML VBox sq31;
-  @FXML VBox sq32;
-  @FXML VBox sq33;
-  @FXML VBox sq34;
-  @FXML VBox sq35;
-  @FXML VBox sq36;
-  @FXML VBox sq37;
-
-  // Row 5
-  @FXML VBox sq40;
-  @FXML VBox sq41;
-  @FXML VBox sq42;
-  @FXML VBox sq43;
-  @FXML VBox sq44;
-  @FXML VBox sq45;
-  @FXML VBox sq46;
-  @FXML VBox sq47;
-
-  // Row 6
-  @FXML VBox sq50;
-  @FXML VBox sq51;
-  @FXML VBox sq52;
-  @FXML VBox sq53;
-  @FXML VBox sq54;
-  @FXML VBox sq55;
-  @FXML VBox sq56;
-  @FXML VBox sq57;
-
-  // Row 7
-  @FXML VBox sq60;
-  @FXML VBox sq61;
-  @FXML VBox sq62;
-  @FXML VBox sq63;
-  @FXML VBox sq64;
-  @FXML VBox sq65;
-  @FXML VBox sq66;
-  @FXML VBox sq67;
-
-  // Row 8
-  @FXML VBox sq70;
-  @FXML VBox sq71;
-  @FXML VBox sq72;
-  @FXML VBox sq73;
-  @FXML VBox sq74;
-  @FXML VBox sq75;
-  @FXML VBox sq76;
-  @FXML VBox sq77;
+  @FXML VBox sq00, sq01, sq02, sq03, sq04, sq05, sq06, sq07;
+  @FXML VBox sq10, sq11, sq12, sq13, sq14, sq15, sq16, sq17;
+  @FXML VBox sq20, sq21, sq22, sq23, sq24, sq25, sq26, sq27;
+  @FXML VBox sq30, sq31, sq32, sq33, sq34, sq35, sq36, sq37;
+  @FXML VBox sq40, sq41, sq42, sq43, sq44, sq45, sq46, sq47;
+  @FXML VBox sq50, sq51, sq52, sq53, sq54, sq55, sq56, sq57;
+  @FXML VBox sq60, sq61, sq62, sq63, sq64, sq65, sq66, sq67;
+  @FXML VBox sq70, sq71, sq72, sq73, sq74, sq75, sq76, sq77;
+  private Square[][] squares;
 
   @FXML
   protected void initialize() {
-    sq00.setOnMouseClicked(this::test);
+    // Instantiate all the squares
+    squares = new Square[][]{
+        { new Square(sq00), new Square(sq01), new Square(sq02), new Square(sq03), new Square(sq04), new Square(sq05), new Square(sq06), new Square(sq07) },
+        { new Square(sq10), new Square(sq11), new Square(sq12), new Square(sq13), new Square(sq14), new Square(sq15), new Square(sq16), new Square(sq17) },
+        { new Square(sq20), new Square(sq21), new Square(sq22), new Square(sq23), new Square(sq24), new Square(sq25), new Square(sq26), new Square(sq27) },
+        { new Square(sq30), new Square(sq31), new Square(sq32), new Square(sq33), new Square(sq34), new Square(sq35), new Square(sq36), new Square(sq37) },
+        { new Square(sq40), new Square(sq41), new Square(sq42), new Square(sq43), new Square(sq44), new Square(sq45), new Square(sq46), new Square(sq47) },
+        { new Square(sq50), new Square(sq51), new Square(sq52), new Square(sq53), new Square(sq54), new Square(sq55), new Square(sq56), new Square(sq57) },
+        { new Square(sq60), new Square(sq61), new Square(sq62), new Square(sq63), new Square(sq64), new Square(sq65), new Square(sq66), new Square(sq67) },
+        { new Square(sq70), new Square(sq71), new Square(sq72), new Square(sq73), new Square(sq74), new Square(sq75), new Square(sq76), new Square(sq77) }
+    };
+
+    // Loop over the squares, giving each of them a copy of the list of squares
+    for (int x = 0; x < 8; x++) {
+      for (int y = 0; y < 8; y++) {
+        squares[y][x].setAllSquares(squares);
+      }
+    }
   }
 
-  @FXML
-  private void test(MouseEvent mouseEvent) {
-    System.out.println("hej");
+  @Override
+  public void postInitialization() {
+    // Give all the squares their corresponding pieces.
+    // We actually want a null pointer exception here if the pieces are not set, that's a serious error.
+    // We should inherit the game from the old controller through GenericController
+    for (Piece piece : game.getPieces()) {
+      int x = piece.getX();
+      int y = piece.getY();
+      squares[y][x].setPiece(piece);
+    }
   }
 }
